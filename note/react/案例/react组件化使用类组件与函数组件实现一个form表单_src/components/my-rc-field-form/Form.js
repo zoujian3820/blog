0a05f12012020e296c930cf97cc6a7877111a3b5
mrzou <382066178@qq.com>
2021-06-27 23:54:41 +0800

    update

--- note/react/案例/react组件化使用类组件与函数组件实现一个form表单_src/components/my-rc-field-form/Form.js
+++ note/react/案例/react组件化使用类组件与函数组件实现一个form表单_src/components/my-rc-field-form/Form.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: mrzou
+ * @Date: 2021-06-27 23:28:33
+ * @LastEditors: mrzou
+ * @LastEditTime: 2021-06-27 23:54:23
+ * @Description: file content
+ */
 import React from "react";
 import useForm from "./useForm";
 import FieldContext from "./FieldContext";
 
 // 2、通过Provider进行值的传递
-export default function Form({children, form, onFinish, onFinishFailed}, ref) {
+export default function Form({ children, form, onFinish, onFinishFailed }, ref) {
+  // 如果是类组件 from为空 则useForm中将走 new FormStore
   const [formInstance] = useForm(form);
 
   React.useImperativeHandle(ref, () => formInstance);
@@ -13,6 +21,7 @@ export default function Form({children, form, onFinish, onFinishFailed}, ref) {
     onFinishFailed,
   });
   return (
+    // 放一个真实form元素 绑定 onsubmit 提交事件
     <form
       onSubmit={(e) => {
         e.preventDefault();
