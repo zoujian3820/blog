e27bcee1d078dda40d8a0f0694a8748685d9ff2e
mrzou <382066178@qq.com>
2022-01-13 23:59:38 +0800

    feat: update

--- note/webpack/从0搭建一个开发环境/webpack.config.js
+++ note/webpack/从0搭建一个开发环境/webpack.config.js
@@ -9,6 +9,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 const { resolve } = require('path')
+// const TerserWebpackPlugin = require('terser-webpack-plugin')
 // 设置nodejs环境变量
 // 此处删除则 webpack 运行时默认走 production
 // 所以会导致 css兼容处理的 browserslist配置取 production
@@ -20,8 +21,13 @@ const { resolve } = require('path')
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: 'js/bundle.js',
-    path: resolve(__dirname, 'build')
+    filename: 'js/[name].[contenthash:10].js',
+    path: resolve(__dirname, 'build'),
+    chunkFilename: 'js/[name].[contenthash:10]_chunk.js' // 非入口chunk的名称
+    // library: '[name]', // 整个库向外暴露的变量名
+    // libraryTarget: 'window' // 变量名添加到哪个上 browser
+    // libraryTarget: 'global' // 变量名添加到哪个上 node
+    // libraryTarget: 'commonjs' // umd amd
     // assetModuleFilename: 'images/[name].[hash:6][ext][query]'
   },
   module: {
@@ -225,6 +231,72 @@ module.exports = {
           }
         ]
       }
+    ]
+  },
+  resolve: {
+    // 配置解析模块路径别名: 优点简写路径 缺点路径没有提示
+    alias: {
+      $css: resolve(__dirname, 'src/css')
+    },
+    // 配置省略文件路径的后缀名
+    extensions: ['.js', '.json', '.jsx', '.css'],
+    // 告诉 webpack 解析模块是去找哪个目录
+    modules: ['node_modules'] // resolve(__dirname, '../../node_modules'),
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+      // 默认值，可以不写~
+      /* minSize: 30 * 1024, // 分割的chunk最小为30kb
+      maxSiza: 0, // 最大没有限制
+      minChunks: 1, // 要提取的chunk最少被引用1次
+      maxAsyncRequests: 5, // 按需加载时并行加载的文件的最大数量
+      maxInitialRequests: 3, // 入口js文件最大并行请求数量
+      automaticNameDelimiter: '~', // 名称连接符
+      name: true, // 可以使用命名规则
+      cacheGroups: {
+        // 分割chunk的组
+        // node_modules文件会被打包到 vendors 组的chunk中。--> vendors~xxx.js
+        // 满足上面的公共规则，如：大小超过30kb，至少被引用一次。
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          // 优先级
+          priority: -10
+        },
+        default: {
+          // 要提取的chunk最少被引用2次
+          minChunks: 2,
+          // 优先级
+          priority: -20,
+          // 如果当前要打包的模块，和之前已经被提取的模块是同一个，就会复用，而不是重新打包模块
+          reuseExistingChunk: true
+        }
+      } */
+    },
+    // 将当前模块的记录其他模块的hash单独打包为一个文件 runtime
+    // 解决：修改a文件导致b文件的contenthash变化
+    runtimeChunk: {
+      name: (entrypoint) => `runtime-${entrypoint.name}`
+    },
+    minimizer: [
+      // 配置生产环境的压缩方案：js和css
+      // new TerserWebpackPlugin({
+      //   // 开启多进程打包
+      //   parallel: true,
+      //   terserOptions: {
+      //     // 删除注释
+      //     output: {
+      //       comments: false,
+      //     },
+      //     compress: {
+      //       // 去除debug、console
+      //       warnings: true,
+      //       drop_debugger: true,
+      //       drop_console: true,
+      //       pure_funcs: ["console.log"], // 移除console
+      //     },
+      //   },
+      // }),
     ]
   },
   plugins: [
