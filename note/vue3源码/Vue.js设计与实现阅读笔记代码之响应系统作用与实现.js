74610802e4bf25f899389806b66ca1d2663b4b1d
qingyun <qingyun@i9i8.com>
2023-08-17 17:27:55 +0800

    实现 Vue.js 中一个非常重要并且非常有特色的能力computed计算属性，懒执行的effect 即lazy 的 effect

--- note/vue3源码/Vue.js设计与实现阅读笔记代码之响应系统作用与实现.js
+++ note/vue3源码/Vue.js设计与实现阅读笔记代码之响应系统作用与实现.js
@@ -48,7 +48,7 @@ function trigger(target, key) {
   // effects && effects.forEach(effectFn => effectFn()) // 删除
 }
 
-const data = {text: 'hello world', foo: 1}
+const data = {bar: 2, foo: 1}
 const obj = new Proxy(data, {
   // 拦截读取操作
   get(target, key) {
@@ -83,19 +83,35 @@ function effect(fn, options = {}) {
     // 当调用 effect 注册副作用函数时，将副作用函数赋值给 activeEffect
     activeEffect = effectFn
     // 在调用副作用函数之前将当前副作用函数压入栈中
-    effectStack.push(effectFn) // 新增
-    fn()
+    effectStack.push(effectFn)
+
+    // 将 fn 的执行结果存储到 res 中
+    const res = fn() // 新增
+
     // 在当前副作用函数执行完毕后，将当前副作用函数弹出栈，并把activeEffect 还原为之前的值
-    effectStack.pop() // 新增
-    activeEffect = effectStack[effectStack.length - 1] // 新增
+    effectStack.pop()
+    activeEffect = effectStack[effectStack.length - 1]
+
+    // 将 res 作为 effectFn 的返回值
+    return res // 新增
   }
   // 将 options 挂载到 effectFn 上
-  effectFn.options = options // 新增
+  effectFn.options = options
   // activeEffect.deps 用来存储所有与该副作用函数相关的依赖集合
   effectFn.deps = []
   // 执行副作用函数
-  effectFn()
+  // effectFn()
+
+  // 只有非 lazy 的时候，才执行
+  if (!options.lazy) { // 新增
+    // 执行副作用函数
+    effectFn()
+  }
+  // 将副作用函数作为返回值返回
+  return effectFn // 新增
+
 }
+
 
 // 定义一个任务队列，Set本身具有去重，同一个effectFn只会增加一次
 const jobQueue = new Set()
@@ -119,23 +135,83 @@ function flushJob() {
 }
 
 
-effect(() => {
-  // console.log(obj.foo, 'zzzz', +new Date())
-  // obj.foo++
-  // console.log(obj.foo, 'qqqq', +new Date())
-  console.log(obj.foo)
-}, {
-  scheduler(fn) {
-    // setTimeout(fn)
+// effect(() => {
+//   // console.log(obj.foo, 'zzzz', +new Date())
+//   // obj.foo++
+//   // console.log(obj.foo, 'qqqq', +new Date())
+//   console.log(obj.foo)
+// }, {
+//   scheduler(fn) {
+//     // setTimeout(fn)
+//
+//     // fn() 不用以下队列处理 直接执行，会log三次分别为 1 2 3
+//     // 每次调度时，将副作用函数添加到 jobQueue 队列中
+//     jobQueue.add(fn)
+//     // 调用 flushJob 刷新队列，执行出的结果，只log两次 1 3 也就是Vue中连续多次修改响应式数据但只会触发一次更新
+//     flushJob()
+//   }
+// })
+//
+// obj.foo++
+// obj.foo++
+// // console.log('结束了')
 
-    // fn() 不用以下队列处理 直接执行，会log三次分别为 1 2 3
-    // 每次调度时，将副作用函数添加到 jobQueue 队列中
-    jobQueue.add(fn)
-    // 调用 flushJob 刷新队列，执行出的结果，只log两次 1 3 也就是Vue中连续多次修改响应式数据但只会触发一次更新
-    flushJob()
+
+// function computed(getter) {
+//   // 把 getter 作为副作用函数，创建一个 lazy 的 effect
+//   const effectFn = effect(getter, {
+//     lazy: true
+//   })
+//   const obj = {
+//     // 当读取 value 时才执行 effectFn
+//     get value() {
+//       return effectFn()
+//     }
+//   }
+//   return obj
+// }
+function computed(getter) {
+  // value 用来缓存上一次计算的值
+  let value
+  // dirty 标志，用来标识是否需要重新计算值，为 true 则意味着“脏”，需要计算
+  let dirty = true
+  const effectFn = effect(getter, {
+    lazy: true,
+    // 添加调度器，在调度器中将 dirty 重置为 true
+    scheduler() {
+      dirty = true
+      // 当计算属性依赖的响应式数据变化时，手动调用 trigger 函数触发响应
+      trigger(obj, 'value')
+    }
+  })
+  const obj = {
+    get value() {
+      // 只有“脏”时才计算值，并将得到的值缓存到 value 中
+      if (dirty) {
+        value = effectFn()
+        // 将 dirty 设置为 false，下一次访问直接使用缓存到 value 中的值
+        dirty = false
+      }
+      console.log('activeEffect', effectFn && effectFn.options)
+      // 当读取 value 时，手动调用 track 函数进行追踪
+      // 此时computed内的effectFn已执行完，
+      // activeEffect为上一个effectFn（调取computed值之前的effectFn）并追踪此时的activeEffect
+      track(obj, 'value')
+      return value
+    }
   }
-})
+  return obj
+}
 
+const sumRes = computed(() => obj.foo + obj.bar)
+
+console.log(sumRes.value) // 3
 obj.foo++
+console.log(sumRes.value) // 4
+
+
+effect(() => {
+  // 在该副作用函数中读取 sumRes.value
+  console.log('computed:', sumRes.value)
+})
 obj.foo++
-// console.log('结束了')
